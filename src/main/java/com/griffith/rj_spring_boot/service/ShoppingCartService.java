170d15044ce6b3a0c0cd57b26fddfd2bf42b1367
Griffith <390239178@qq.com>
2023-02-01 20:53:02 +0800

    name reformed

--- src/main/java/com/griffith/rj_spring_boot/service/ShoppingCartService.java
+++ src/main/java/com/griffith/rj_spring_boot/service/ShoppingCartService.java
@@ -3,5 +3,5 @@ package com.griffith.rj_spring_boot.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.griffith.rj_spring_boot.entity.ShoppingCart;
 
-public interface ShoppinpCartService extends IService<ShoppingCart> {
+public interface ShoppingCartService extends IService<ShoppingCart> {
 }
--- src/main/java/com/griffith/rj_spring_boot/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/griffith/rj_spring_boot/service/impl/ShoppingCartServiceImpl.java
@@ -3,9 +3,9 @@ package com.griffith.rj_spring_boot.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.griffith.rj_spring_boot.entity.ShoppingCart;
 import com.griffith.rj_spring_boot.mapper.ShoppingCartMapper;
-import com.griffith.rj_spring_boot.service.ShoppinpCartService;
+import com.griffith.rj_spring_boot.service.ShoppingCartService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ShoppingCartServiceImpl extends ServiceImpl<ShoppingCartMapper, ShoppingCart> implements ShoppinpCartService {
+public class ShoppingCartServiceImpl extends ServiceImpl<ShoppingCartMapper, ShoppingCart> implements ShoppingCartService {
 }
