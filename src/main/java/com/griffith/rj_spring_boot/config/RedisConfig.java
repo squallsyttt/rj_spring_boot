fc5932523bfc788359896e2d2924204d4feb3905
Griffith <390239178@qq.com>
2023-02-07 10:00:09 +0800

    redis config init

---
+++ src/main/java/com/griffith/rj_spring_boot/config/RedisConfig.java
@@ -0,0 +1,20 @@
+package com.griffith.rj_spring_boot.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object,Object> redisTemplate = new RedisTemplate<>();
+        //默认的key 序列化器 jdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
